f11a539375837c7767168fcc2f8c85401c9fd436
Ahmed Trabelsi <trabelsig37@gmail.com>
2023-05-23 11:36:33 +0100

    add grid pane

--- src/main/java/com/examen/SampleController.java
+++ src/main/java/com/examen/SampleController.java
@@ -1,13 +1,29 @@
 package com.examen;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 
-public class SampleController {
+public class SampleController implements Initializable {
+  private double value = 40;
+  private double converted;
+  String[] choix = { "From celsius to Fahrenheit", "From Fahrenheit to celsius" };
+  @FXML
+  ComboBox<String> listeChoix;
 
   @FXML
   private void Convert() throws IOException {
-    System.out.println("hello world !");
+    converted = (value - 32) / 1.8;
+
+    System.out.println(converted);
+  }
+
+  @Override
+  public void initialize(URL arg0, ResourceBundle arg1) {
+    listeChoix.getItems().setAll(choix);
   }
 }
